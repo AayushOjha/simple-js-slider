28b5780be4c70ef29641530d05a10111a744181a
Ayush Ojha <18EGJCS024@GITJAIPUR.COM>
2021-12-26 15:22:20 +0530

    finished basic slider with infinit loop

--- app.js
+++ app.js
@@ -11,13 +11,31 @@ const size = allImgs[1].clientWidth;
 lonSlide.style.transform = "translateX(" + -size * counter + "px)";
 
 nxtBtn.addEventListener('click',()=>{
+  if(counter >= allImgs.length -1) return; 
   lonSlide.style.transition = "transform 0.4s ease-in-out";
   counter++;
   lonSlide.style.transform = "translateX(" + -size * counter + "px)";
 })
 
 preBtn.addEventListener('click',()=>{
+  if (counter <= 0) return;
   lonSlide.style.transition = "transform 0.4s ease-in-out";
   counter--;
   lonSlide.style.transform = "translateX(" + -size * counter + "px)";
 })
+
+lonSlide.addEventListener('transitionend',()=>{
+  if (allImgs[counter].id == "lastone") {
+    lonSlide.style.transition = "none";
+    counter = allImgs.length -2;
+    lonSlide.style.transform = "translateX("+ (-size * counter) + "px)";
+  }
+
+  if (allImgs[counter].id == "firstone") {
+    lonSlide.style.transition = "none";
+    counter = 1;
+    lonSlide.style.transform = "translateX("+ (-size * counter) + "px)";
+}
+
+})
+
